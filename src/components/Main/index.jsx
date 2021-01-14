a573839c31a35068c071f51b99712170ab37fc39
KozaX <55134615+KozaX@users.noreply.github.com>
2021-01-14 13:30:57 +0100

    add currency select field

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -6,7 +6,14 @@ const Main = () => {
   return (
     <main>
       <div className="inputs-container">
-        <input type="number" className="input-amount" />
+        <input type="number" className="input-amount" placeholder="0.00" />
+        <select
+          name="currency"
+          id="currency-select"
+          className="currency-select"
+        >
+          <option value="USD">USD</option>
+        </select>
       </div>
       <p>Enter an amount to check the rates</p>
     </main>
